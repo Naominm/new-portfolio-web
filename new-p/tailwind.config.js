d73ee5ed7d0ce3ba0fc8c217fdced94948ad2784
Naomi Mbugua <naomimbugua349@gmail.com>
2024-07-20 22:43:17 +0300

    updated the bg css

--- new-p/tailwind.config.js
+++ new-p/tailwind.config.js
@@ -1,12 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./public/index.html",
-  ],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+   
+    container:{
+      padding:{
+        default:'15px'
+      }
+    },
+    screens:{
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    
+    extend: {
+      colors:{
+        primary:'#0a0a0a',
+        accent:'#b809c3',
+      },
+      backgroundImage:{
+        site:"url('./assets/site-bg.jpg')",
+      },
+    },
   },
   plugins: [],
 }
-
