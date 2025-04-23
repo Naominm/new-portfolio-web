45234f185f3a2729b46073b63272b88719c0aadf
Naominm <naomimbugua536@gmail.com>
2025-04-23 09:58:57 +0300

    Add project skillset

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,18 +1,18 @@
 // src/data/skills.js
 import { FaJs, FaReact, FaGit, FaGithub } from 'react-icons/fa';
-import { SiTailwindcss, SiNextdotjs, SiMicrosoftazure } from 'react-icons/si';
-import { SiFigma } from 'react-icons/si'; // Figma icon
-import { FaPython } from 'react-icons/fa'; // Python icon
+import { SiNodedotjs,SiPostgresql,SiMongodb,SiTypescript,SiExpress } from 'react-icons/si';
+
 
 const skills = [
     { name: 'JS', icon: <FaJs />, color: '#f7df1e' },
     { name: 'React', icon: <FaReact />, color: '#61dafb' },
-    { name: 'Tailwind ', icon: <SiTailwindcss />, color: '#38b2ac' },
-    { name: 'Next.js', icon: <SiNextdotjs />, color: '#000000' },
-    { name: 'Azure', icon: <SiMicrosoftazure />, color: '#0078d4' },
+    { name: 'Typescript ', icon: <SiTypescript />, color: '#38b2ac' },
+    { name: 'NodeJs', icon: <SiNodedotjs />, color: '#F24E1E' }, 
+    { name: 'Express', icon: <SiExpress />, color: '#F24E1E' }, 
+    { name: 'PostgreSQL', icon: <SiPostgresql />, color: '#000000' },
+    { name: 'MongoDB', icon: <SiMongodb />, color: '#0078d4' },
     { name: 'Git', icon: <FaGit />, color: '#f05032' },
     { name: 'GitHub', icon: <FaGithub />, color: '#181717' },
-    { name: 'Figma', icon: <SiFigma />, color: '#F24E1E' }, 
    
 ];
 
