cccceb91469d14c9bab5770700091328011b5e5f
Naominm <naomimbugua536@gmail.com>
2025-04-23 15:30:29 +0300

    Correct path images

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import mainImage from '../assets/g2.jpg';  // Ensure this path is correct
+import mainImage from '../assets/third.jpg';  // Ensure this path is correct
 import { FaGithub, FaLinkedin, FaWhatsapp } from 'react-icons/fa';
 import { TypeAnimation } from 'react-type-animation';
 import AOS from 'aos';
